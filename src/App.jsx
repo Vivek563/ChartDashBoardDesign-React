1fb9f8c16378a39a1ef14da8bb4275131a04e24a
Vivek maurya <vivek563maurya@gmail.com>
2023-09-14 22:40:51 +0530

    "initial commit"

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Signin from "./components/Signin";
+import Card from "./components/Card.jsx";
+
+function App() {
+  return (
+    <>
+      {/* <Signin /> */}
+      <Card />
+    </>
+  );
+}
+
+export default App;
